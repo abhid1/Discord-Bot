70fc0bcaecc449382de5bc2f5418b6c08de19e14
Abhi <abhidubal1@gmail.com>
2019-07-10 23:21:43 -0600

    Switched from vanilla javascript to discord.js, need to add many features.

--- bot.js
+++ bot.js
@@ -1,71 +1,47 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+const Discord = require('discord.js');
 var auth = require('./auth.json');
-var commands = ["hello", "buckets", "kawhinot", "rng"];
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const client = new Discord.Client();
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+let prefix = '!';
+
+var commands = ["ping", "beep", "hello", "buckets", "kawhinot", "rng"];
+
+client.on('message', message => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+        message.channel.send('Pong.');
+    } 
+    else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send('Boop.');
+    }
+    else if (message.content.startsWith(`${prefix}hello`)) {
+        message.channel.send('Hola.');
+        setTimeout(() => {
+            message.channel.send('Bonjour.');
+            }, 500);
+        setTimeout(() => {
+            message.channel.send('Ni Hao.');
+            }, 500);
+    }
+    else if (message.content.startsWith(`${prefix}buckets`)) {
+        message.channel.send('Board man gets paid.');
+    }
+    else if (message.content.startsWith(`${prefix}kawhinot`)) {
+        message.channel.send('https://youtu.be/5oHrcAdThNk?t=275');
+    }
+    else if (message.content.startsWith(`${prefix}rng`)) {
+        var randomNum = Math.floor(Math.random() * 100);
+        message.channel.send("RANDOM NUMBER: " + randomNum);
+    }
+    else if (message.content.startsWith(`${prefix}commands`)) {
+        var command = "List of commands:\n";
+        for (var i = 0; i < commands.length; i++) {
+            command += "!" + commands[i] + "\n";
+        }
+        message.channel.send(command);
+    }
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case commands[0]:
-                botMessage("hola", channelID);
-                setTimeout(() => {
-                    botMessage("bonjour", channelID);
-                }, 500);
-                setTimeout(() => {
-                    botMessage("ni hao", channelID);
-                }, 500);
-            break;
-            case commands[1]:
-                botMessage("Board man gets paid.", channelID);
-            break;
-            case commands[2]:
-                botMessage("https://youtu.be/5oHrcAdThNk?t=275", channelID);
-            break;
-            case "commands":
-                var command;
-                command = "List of commands:\n";
-                for (var i = 0; i < commands.length; i++) {
-                    command += "!" + commands[i] + "\n";
-                }
-                botMessage(command, channelID);
-            case commands[3]:
-                var randomNum = Math.floor(Math.random() * 100);
-                botMessage("RANDOM NUMBER: " + randomNum, channelID);
-         }
-     }
-});
-
-function botMessage(messageFromBot, channelID)
-{
-    bot.sendMessage({
-        to: channelID,
-        message: messageFromBot
-    });
-}
-
-
+client.login(auth.token);
